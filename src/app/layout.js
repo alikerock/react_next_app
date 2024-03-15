d2ad0a7fcea7e65cb1511386d37f604968184e01
ezwebpublishing <ezwebpub@gmail.com>
2024-03-15 23:30:04 +0900

    layout.js 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <nav>
+          <ol>
+            <li><a href="/read/1">html</a></li>
+            <li><a href="/read/2">css</a></li>
+            <li><a href="/read/3">javascript</a></li>
+          </ol>
+        </nav>
+        {children}
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/update/1">Update</a></li>
+          <li><button>delete</button></li>
+        </ul>
+
+        </body>
     </html>
   );
 }
