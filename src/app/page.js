45cb3dbbeda07a845bae9123e010db061cb6e3ca
ezwebpublishing <ezwebpub@gmail.com>
2024-03-16 23:14:27 +0900

    Image add

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <h2>Welcome</h2>
       <p>Hello, WEB!</p>
+      <Image src="/home_icon.png" alt="Home Icon" width={48} height={48}></Image>
     </>
   );
 }
