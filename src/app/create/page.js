60c7d09d88ae5d514a59e64701909bf767aa56b6
ezwebpublishing <ezwebpub@gmail.com>
2024-03-17 16:41:52 +0900

    create post

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,6 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+  const router = useRouter();
   return (
-    <form>
+    <form onSubmit={(e)=>{
+      e.preventDefault();
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+      const options = {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({title, body})
+      }
+      fetch(`http://localhost:9999/topics`, options)
+        .then(res=>res.json())
+        .then(result=>{
+          console.log(result);
+          const lastid = result.id;
+          router.push(`/read/${lastid}`);
+        });
+    }}>
       <p>
         <input type="text" name="title" placeholder="title"/>
       </p>
@@ -13,3 +36,4 @@ export default function Create() {
     </form>
   );
 }
+
