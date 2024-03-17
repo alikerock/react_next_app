99c868092e8941e8f43fb95ac42cae9e43030062
ezwebpublishing <ezwebpub@gmail.com>
2024-03-17 15:47:09 +0900

    글 생성 폼 생성

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,15 @@
 export default function Create() {
   return (
-    <>
-      Create !
-    </>
+    <form>
+      <p>
+        <input type="text" name="title" placeholder="title"/>
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>        
+      </p>  
+      <p>
+        <button>전송</button>
+      </p>
+    </form>
   );
 }
